365901ed1db03a42e8a10df322492976d9753247
karayilanberat <karayilanberat@gmail.com>
2024-07-20 03:00:48 +0300

    Initial Commit

---
+++ DiceeSwifUI/DiceeSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwifUIApp.swift
+//  DiceeSwifUI
+//
+//  Created by berat on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
